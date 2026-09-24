eaf7d561b7f648c8fa5c8149d0416ecc86428193
Nika <yeet.datdicc@gmail.com>
2026-09-24 14:59:01 +0200

    adding options to the script

--- minibak.sh
+++ minibak.sh
@@ -1,13 +1,42 @@
 #! /bin/bash
 
-defaultBackDir=$(echo ~)
+#defaultBackDir=$(echo ~)
+bakDir=""
+verboseMode=0
 
-if [ -n $(grep backupDir conf) ]
-then
-	grep '#' $(grep backupDir conf)
+helpText=$(cat << EOT
+Usage: $0 [ options ] -d <directory>
+
+Options:
+-d - Directory to backup
+-v - Verbose; Debug messages of what's being done
+-h - Show this help message
+EOT
+)
+
+#if [ -n $(grep backupDir conf) ]
+#then
+#	grep '#' $(grep backupDir conf)
+#fi
+
+#superIdol="test!"
+
+#echo "$superIdol"
+#echo "$defaultBackDir"
+
+while getopts "d:vh" flag; do
+	echo "flag -$flag, arg $OPTARG";
+	case $flag in
+		d) bakDir=$OPTARG ;;
+		v) verboseMode=1 ;;
+		h) echo "$helpText" >&2; exit 0 ;;
+		\?) echo "Unknown option: -$flag" >&2; exit 1;;
+	esac
+done
+
+if [[ -z $bakDir ]]; then
+	echo "$helpText" >&2
+	exit 1
 fi
 
-superIdol="test!"
-
-echo "$superIdol"
-echo "$defaultBackDir"
+if [[ "$verboseMode" -eq 1 ]] then echo "dir = $bakDir"; fi
